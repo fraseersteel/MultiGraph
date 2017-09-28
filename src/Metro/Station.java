ccbbe463adfb237cc0974dfd2053c97a2343f1f6
xbb15134 <kuen.chan.2015@uni.strath.ac.uk>
2017-09-28 18:45:32 +0100

    constructor now sets class' field for ID

--- src/Metro/Station.java
+++ src/Metro/Station.java
@@ -7,20 +7,21 @@ import java.util.Set;
 
 public class Station implements INode {
 
-
     private String name;
     private int id;
 
-    public Station(int id, String name){
+    public Station(int id, String name) {
+        this.id = id;
         this.name = name;
     }
+
     @Override
     public int getId() {
         return id;
     }
 
     @Override
-    public String toString(){
+    public String toString() {
         return name;
     }
 
