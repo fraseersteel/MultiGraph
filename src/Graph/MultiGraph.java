c66610dc2a623e1829d7c64af7448145904a4a8a
xbb15134 <kuen.chan.2015@uni.strath.ac.uk>
2017-09-28 14:56:14 +0100

    Testing git push

--- src/Graph/MultiGraph.java
+++ src/Graph/MultiGraph.java
@@ -7,26 +7,33 @@ public class MultiGraph implements IMultigraph {
 
     @Override
     public boolean addNode(INode node) {
+
         return false;
     }
 
     @Override
     public boolean removeNode(INode node) {
+
         return false;
     }
 
     @Override
     public boolean addEdge(IEdge edge) {
+
         return false;
     }
 
     @Override
     public boolean removeEdge(IEdge edge) {
+
         return false;
     }
 
     @Override
     public List<IEdge> getRoute(INode node1, INode node2) {
+
         return null;
     }
+
+
 }
