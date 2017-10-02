a18dc6b32d3c7b818d5a14b6be4bc23ae803c140
xbb15134 <kuen.chan.2015@uni.strath.ac.uk>
2017-10-02 20:26:42 +0100

    reimplemented getRoute to supply shortest path. Does not calculate for route changes. Uses Breadth first search approach, a list to store the optimal parent for each node.

--- src/Graph/MultiGraph.java
+++ src/Graph/MultiGraph.java
@@ -1,7 +1,9 @@
 package Graph;
 
 import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
+import java.util.Queue;
 
 public class MultiGraph implements IMultigraph {
 
@@ -14,18 +16,17 @@ public class MultiGraph implements IMultigraph {
     }
 
 
-
     @Override
     public boolean addNode(INode node) {
 
 
-            for (int i = 0; i < nodeList.size(); i++) {
-                if (node.getId() == nodeList.get(i).getId()) {
-                    return false;
-                }
+        for (int i = 0; i < nodeList.size(); i++) {
+            if (node.getId() == nodeList.get(i).getId()) {
+                return false;
             }
-            nodeList.add(node);
-            return true;
+        }
+        nodeList.add(node);
+        return true;
 
     }
 
@@ -37,12 +38,66 @@ public class MultiGraph implements IMultigraph {
 
 
     public List<IEdge> getRoute(INode node1, INode node2) {
+        //List<INode> visited = new ArrayList<INode>();
+        //List<IEdge> currentEdgeSequence = new ArrayList<IEdge>();
+        //List<IEdge> listofstations = method2(node1, node2, visited, currentEdgeSequence);
+
+        //return listofstations;
+        INode destination = node2;
+        INode source = node1;
+
         List<INode> visited = new ArrayList<INode>();
-        List<IEdge> currentEdgeSequence = new ArrayList<IEdge>();
-        List<IEdge> listofstations = method2(node1, node2, visited, currentEdgeSequence);
+        Queue<INode> queue = new LinkedList<INode>();
+
+        List<parentNodeRecord> parents = new ArrayList<parentNodeRecord>();
+
+        visited.add(node1);
+        queue.add(node1);
+        while (!queue.isEmpty()) {
+            INode temp = queue.poll();
+            if (temp.getId() == node2.getId()) {
+                break;
+            }
+
+            List<IEdge> successors = successors2(temp);
+
+            for (IEdge i : successors) {
+                if (i.getNode1().getId() == temp.getId()) {
+                    if (!visited.contains(i.getNode2())) {
+                        INode child = i.getNode2();
+                        queue.add(child);
+                        parents.add(new parentNodeRecord(child, i.getNode1(), i));
+                    }
+                } else {
+                    if (!visited.contains(i.getNode1())) {
+                        INode child = i.getNode1();
+                        queue.add(child);
+                        parents.add(new parentNodeRecord(child, i.getNode2(), i));
+                    }
+
+                }
+            }
 
 
-        return listofstations;
+        }
+
+
+        List<IEdge> edgeSequence = new ArrayList<IEdge>();
+
+
+        System.out.println("!!!!");
+        while (destination.getId() != source.getId()) {
+            System.out.println(destination.getId());
+            for (int i = 0; i< parents.size(); i++) {
+                if (parents.get(i).getNode().getId() == destination.getId()) {
+                    edgeSequence.add(0, parents.get(i).getEdge());
+                    destination = parents.get(i).getParent();
+                    break;
+                }
+            }
+        }
+
+        return edgeSequence;
     }
 
     @Override
@@ -57,6 +112,7 @@ public class MultiGraph implements IMultigraph {
     * If no matching ID, returns null
      */
     public INode getNode(int ID) {
+
         for (int i = 0; i < nodeList.size(); i++) {
             if (nodeList.get(i).getId() == ID) {
 
@@ -67,50 +123,10 @@ public class MultiGraph implements IMultigraph {
         return null;
     }
 
-    private List<IEdge> method2(INode node1, INode node2, List<INode> visited, List<IEdge> currentEdgeChain) {
-        ArrayList<IEdge> edgeChain = new ArrayList<IEdge>();
-        visited.add(node1);
-        if (node1.getId() == node2.getId()) {
-
-            return (currentEdgeChain);
-        } else if (successors2(node1).size() == 0) {
-            edgeChain = new ArrayList<IEdge>();
-            return(edgeChain);
-        }
-        else if (successors2(node1).size() >= 1) {
-            for (int i = 0; i < successors2(node1).size(); i++) {
-                INode successiveNode;
-
-
-
-                if (successors2(node1).get(i).getNode1().getId() == node1.getId()) {
-                    successiveNode = successors2(node1).get(i).getNode2();
-                } else {
-                    successiveNode = successors2(node1).get(i).getNode1();
-                }
-
-                List<IEdge> temporary = new ArrayList<IEdge>();
-                temporary.add(successors2(node1).get(i));
-                if (!visited.contains(successiveNode)) {
-                    temporary = method2(successiveNode, node2, visited,temporary);
-
-                    if (temporary.size() > 0) {
-                        currentEdgeChain.addAll(temporary);
-                        return(currentEdgeChain);
-                    }
-                }
-
-            }
-        }
-        edgeChain = new ArrayList<IEdge>();
-        return(edgeChain);
-    }
-
 
     private List<IEdge> successors2(INode node) {
         ArrayList<IEdge> successorList = new ArrayList<IEdge>();
         int sourceID = node.getId();
-
 
 
         for (int i = 0; i < edgeList.size(); i++) {
@@ -125,5 +141,29 @@ public class MultiGraph implements IMultigraph {
 
         return successorList;
     }
-    
+
+    private class parentNodeRecord {
+        private INode node;
+        private INode parent;
+        private IEdge edge;
+
+        public parentNodeRecord(INode node, INode parent, IEdge edge) {
+            this.node = node;
+            this.parent = parent;
+            this.edge = edge;
+        }
+
+        public INode getNode() {
+            return node;
+        }
+
+        public INode getParent() {
+            return parent;
+        }
+
+        public IEdge getEdge() {
+            return edge;
+        }
+    }
+
 }
