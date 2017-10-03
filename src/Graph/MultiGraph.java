62e6dbb4fa1eeba83909c94049c8f5a4d7ab6769
xbb15134 <kuen.chan.2015@uni.strath.ac.uk>
2017-10-03 21:01:26 +0100

    now uses hashset to store the parent for each node within the shortest path. (Instead of arraylist)

--- src/Graph/MultiGraph.java
+++ src/Graph/MultiGraph.java
@@ -6,6 +6,8 @@ import java.util.List;
 import java.util.Queue;
 import java.util.Set;
 import java.util.HashSet;
+import java.util.Map;
+import java.util.HashMap;
 
 public class MultiGraph implements IMultigraph {
 
@@ -44,30 +46,42 @@ public class MultiGraph implements IMultigraph {
         List<INode> visited = new ArrayList<>();
         Queue<INode> queue = new LinkedList<>();
 
-        List<ParentNodeRecord> parents = new ArrayList<>();
+        HashMap<INode, ParentNodeRecord> parents = new HashMap<>();
 
         visited.add(node1);
         queue.add(node1);
         while (!queue.isEmpty()) {
-            INode temp = queue.poll();
-            if (temp.getId() == node2.getId()) {
+            INode curNodeToCheck = queue.poll();
+
+            if (curNodeToCheck.getId() == node2.getId()) {
                 break;
             }
- 
-            List<IEdge> successors = successors(temp);
+
+            List<IEdge> successors = successors(curNodeToCheck);
 
             for (IEdge i : successors) {
-                if (i.getNode1().getId() == temp.getId()) {
+                if (i.getNode1().getId() == curNodeToCheck.getId()) {
                     if (!visited.contains(i.getNode2())) {
                         INode child = i.getNode2();
                         queue.add(child);
-                        parents.add(new ParentNodeRecord(child, i.getNode1(), i));
+
+
+                        if (parents.get(child) == null) {
+                            parents.put(child, new ParentNodeRecord(curNodeToCheck, i));
+                        }
+
+
                     }
-                } else {
+                } else if (i.getNode2().getId() == curNodeToCheck.getId()) {
                     if (!visited.contains(i.getNode1())) {
+
                         INode child = i.getNode1();
                         queue.add(child);
-                        parents.add(new ParentNodeRecord(child, i.getNode2(), i));
+
+
+                        if (!parents.containsKey(child)) {
+                            parents.put(child, new ParentNodeRecord(curNodeToCheck, i));
+                        }
                     }
 
                 }
@@ -77,18 +91,14 @@ public class MultiGraph implements IMultigraph {
 
         List<IEdge> edgeSequence = new ArrayList<>();
 
+
         while (node1.getId() != destination.getId()) {
-            System.out.println(destination.getId());
-
-
-            for (ParentNodeRecord i : parents) {
-                if (i.getNode().getId() == destination.getId()) {
-                    edgeSequence.add(0, i.getEdge());
-                    destination = i.getParent();
-                    break;
-                }
-            }
+            ParentNodeRecord curDestinationRecord = parents.get(destination);
+            edgeSequence.add(curDestinationRecord.getEdge());
+            destination = curDestinationRecord.getParent();
         }
+
+
 
         return edgeSequence;
     }
@@ -136,18 +146,12 @@ public class MultiGraph implements IMultigraph {
     }
 
     class ParentNodeRecord {
-        private INode node;
         private INode parent;
         private IEdge edge;
 
-        ParentNodeRecord(INode node, INode parent, IEdge edge) {
-            this.node = node;
+        ParentNodeRecord(INode parent, IEdge edge) {
             this.parent = parent;
             this.edge = edge;
-        }
-
-        INode getNode() {
-            return node;
         }
 
         INode getParent() {
